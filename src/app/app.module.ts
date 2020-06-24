e712a96eb1fcb47bae86aae4259bcce44f314e82
Andrei Voicu <dreivoicu@gmail.com>
2020-06-24 19:40:57 +0300

    content projection heading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeadingComponent } from './heading/heading.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadingComponent
   ],
   imports: [
     BrowserModule
