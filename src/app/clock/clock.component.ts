83a479da86863dc32d952e0db3ccf78374baf063
Andrei Voicu <dreivoicu@gmail.com>
2020-06-24 21:06:16 +0300

    toggle expander

--- src/app/clock/clock.component.ts
+++ src/app/clock/clock.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-clock',
   templateUrl: './clock.component.html'
 })
-export class ClockComponent implements OnInit {
+export class ClockComponent implements OnInit, OnDestroy {
   currentTime: string;
 
   ngOnInit(): void {
     const date = new Date();
     this.currentTime = `${date.getHours()}:${date.getMinutes()}`;
+
+    console.log('ClockComponent: ngOnInit called');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ClockComponent: ngOnDestroy called');
   }
 }
