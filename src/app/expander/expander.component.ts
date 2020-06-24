e3b100fde12dcc1bb3c52caf42bd9a6ca05d20a0
Andrei Voicu <dreivoicu@gmail.com>
2020-06-24 20:56:09 +0300

    basic expander

--- src/app/expander/expander.component.ts
+++ src/app/expander/expander.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-expander',
   templateUrl: './expander.component.html',
   styleUrls: ['./expander.component.scss']
 })
-export class ExpanderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ExpanderComponent {
+  @Input() heading: string;
 }
